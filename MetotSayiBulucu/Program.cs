ced4c3596473c4a26e6317e1b43064d3b5dba521
selahaddin03 <149465780+selahaddin03@users.noreply.github.com>
2023-12-05 19:36:55 +0300

    Proje dosyası ekle.

---
+++ MetotSayiBulucu/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetotSayiBulucu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayiBulucu sayilar = new SayiBulucu();
+            double bs = sayilar.BuyukSayiyiBul(4, 40);
+            Console.WriteLine("Büyük sayı:"+bs);
+        }
+    }
+    public class SayiBulucu
+    {
+        public double BuyukSayiyiBul(double sayi1,double sayi2)
+        {
+            double sonuc = 0;
+            if(sayi1 >= sayi2) 
+            {
+                sonuc = sayi1;
+            }
+            else
+            {
+                sonuc =  sayi2;
+            }
+            return sonuc;
+        }   
+    }
+}
